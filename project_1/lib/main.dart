199950a5431c3ef8e69645ffb9fc7044be03686c
Anthony Huynh <ahuynh0730@gmail.com>
2020-01-19 12:29:04 -0600

    Title Change

--- project_1/lib/main.dart
+++ project_1/lib/main.dart
@@ -29,7 +29,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Random Name Generator'),
         actions: <Widget> [
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
         ],
