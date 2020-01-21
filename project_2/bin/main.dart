7496283f68ee36363d517a8aae6387a1e42d2a17
Anthony Huynh <ahuynh0730@gmail.com>
2020-01-21 15:04:08 -0600

    Update main.dart
    Removed some nesting and made a function call to create and return a list of planets

--- project_2/bin/main.dart
+++ project_2/bin/main.dart
@@ -3,12 +3,21 @@ import 'package:project_2/project_2.dart';
 void main(List<String> arguments) {
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
-      name: 'Solar System', planets: [
-        Planet(
-          name: 'Mercury',
-          description: 'A very hot planet, closest to the sun.'
-        )
-      ]
+      name: 'Solar System',
+      planets: mockPlanets()
     )
   ).start(); 
+}
+
+List<Planet> mockPlanets(){
+  return [
+    Planet(
+      name: 'Mercury',
+      description: 'A very hot planet, closest to the sun.'
+    ),
+    Planet(
+      name: 'Venus',
+      description: 'It\'s very ccloudy here!'
+    )
+  ];
 }
