8130cfa6db6596a26dbee10eab59e4991f2bc8b4
Anthony Huynh <ahuynh0730@gmail.com>
2020-01-21 14:49:20 -0600

    Update main.dart
    Created one planet

--- project_2/bin/main.dart
+++ project_2/bin/main.dart
@@ -1,5 +1,14 @@
 import 'package:project_2/project_2.dart';
 
 void main(List<String> arguments) {
-  SpaceAdventure(planetarySystem: PlanetarySystem(name: 'Solar System')).start(); 
+  SpaceAdventure(
+    planetarySystem: PlanetarySystem(
+      name: 'Solar System', planets: [
+        Planet(
+          name: 'Mercury',
+          description: 'A very hot planet, closest to the sun.'
+        )
+      ]
+    )
+  ).start(); 
 }
