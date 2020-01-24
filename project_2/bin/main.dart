c5869df5e08dd454c56deaad635ecfcb9712d74e
Anthony Huynh <ahuynh0730@gmail.com>
2020-01-24 15:58:30 -0600

    Update main.dart
    System name and planets data is read from json

--- project_2/bin/main.dart
+++ project_2/bin/main.dart
@@ -2,41 +2,26 @@ import 'package:project_2/project_2.dart';
 import 'dart:io';
 import 'dart:convert';
 
-const systemName = 'Solar System';
-const planetData = {
-  'Mercury': 'A very hot planet, closest to the sun.',
-  'Venus': 'It\'s very cloudy here.',
-  'Earth': 'There is something very familiar about this planet.',
-  'Mars': 'Known as the red planet.',
-  'Jupiter': 'A gas giant, with a noticeable red spot.',
-  'Saturn': 'This planet has beautiful rings around it.',
-  'Uranus': 'Strangely this planet rotates around on its side.',
-  'Neptune': 'A very cold planet, furthest from the sun.',
-  'Pluto': 'I don\'t care what they say - it\'s a planet.'
-};
-
 void main(List<String> arguments) async{
   
   //waits for file content
   var fileContent = await readFromFile(arguments[0]);
-  
 
-  
+  //extract planet data from json
+  //var planetObjsJson = jsonDecode(fileContent)['planets'] as List;
+  //List planetData = planetObjsJson.map((planetJson) => Planet.fromJson(planetJson)).toList();
 
-  /*
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
-      name: systemName,
-      planets: mockPlanets()
+      name: jsonDecode(fileContent)['name'] as String,
+      planets: (
+        jsonDecode(fileContent)['planets'] as List
+      ).map((planetJson) => Planet.fromJson(planetJson)).toList()
     )
-  ).start();*/ 
+  ).start();
+
 }
 
-List<Planet> mockPlanets(){
-  return planetData.entries.map( 
-    (e) => Planet(name:e.key, description: e.value)
-  ).toList();
-}
 
 Future<String> readFromFile(String fileName) async{
   var file = File(fileName);
