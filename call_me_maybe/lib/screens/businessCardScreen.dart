40372c09206213535f2e9e4634259230fc80d4c9
Anthony Huynh <ahuynh0730@gmail.com>
2020-02-14 13:46:27 -0600

    Business Card update
    Shows proper text

--- call_me_maybe/lib/screens/businessCardScreen.dart
+++ call_me_maybe/lib/screens/businessCardScreen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BusinessCardScreen extends StatelessWidget {
 
+  final phoneNumber = '314-707-6765';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Business Card')
+    return Column(
+     children: <Widget>[
+        Text('Anthony Huynh'),
+        Text('Software Engineer'),
+        RaisedButton(
+          onPressed: _sendSMS,
+          child: Text(phoneNumber),
+        ),
+        Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('github.com/ahuynh0730'),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text('ahuynh0730@gmail.com'),
+            ),
+          ]
+        ),
+     ]
     );
   }
+}
+
+_sendSMS() async {
+   
 }
