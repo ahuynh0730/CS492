d8bab63b9714ddfeb6d25672163d29c3e1b73d35
Anthony Huynh <ahuynh0730@gmail.com>
2020-03-02 13:30:20 -0600

    Update customTextFormField.dart

--- journal/lib/widgets/customTextFormField.dart
+++ journal/lib/widgets/customTextFormField.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+
 class CustomTextFormField extends StatefulWidget {
-  CustomTextFormField({Key key, this.labelName}) : super(key: key);
+  CustomTextFormField({Key key, this.labelName, this.journalEntryFields}) : super(key: key);
 
   final String labelName;
+  final journalEntryFields;
 
   @override
   _CustomTextFormFieldState createState() => _CustomTextFormFieldState();
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
+
   @override
   Widget build(BuildContext context) {
     return Padding(
