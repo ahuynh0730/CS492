9e98cd110b7cefee404fd2e4a77c676421204d2c
Anthony Huynh <ahuynh0730@gmail.com>
2020-03-01 23:39:23 -0600

    Create customTextFormField.dart

---
+++ journal/lib/widgets/customTextFormField.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  CustomTextFormField({Key key, this.labelName}) : super(key: key);
+
+  final String labelName;
+
+  @override
+  _CustomTextFormFieldState createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        autofocus: true,
+        decoration: InputDecoration(
+          labelText: widget.labelName,
+          border: OutlineInputBorder(),
+        ),
+        onSaved: (value) {
+
+        },
+        validator: (value) {
+          if(value.isEmpty){
+            return 'Field cannot be empty';
+          } else return null;
+        },
+      ),
+    );
+  }
+}
