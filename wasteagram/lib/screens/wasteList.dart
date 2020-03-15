5b04d6be3399e0fb06a000102a45cbd2bec21257
Anthony Huynh <ahuynh0730@gmail.com>
2020-03-15 14:46:22 -0500

    Lifted body of scaffold out from waste list

--- wasteagram/lib/screens/wasteList.dart
+++ wasteagram/lib/screens/wasteList.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:wasteagram/widgets/scaffoldBody.dart';
 
 
 class WasteList extends StatefulWidget {
@@ -23,127 +24,26 @@ class _WasteListState extends State<WasteList> {
     }
   }
 
-  String getWeekday(day){
-    switch (day) {
-      case 1:
-        return 'Monday';
-        break;
-      case 2:
-        return 'Tuesday';
-        break;
-      case 3:
-        return 'Wednesday';
-        break;
-      case 4:
-        return 'Thursday';
-        break;
-      case 5:
-        return 'Friday';
-        break;
-      case 6:
-        return 'Saturday';
-        break;
-      case 7:
-        return 'Sunday';
-        break;
-      default:
-        return 'Not valid';
-        break;
-    }
-  }
-
-  String getMonth(day){
-    switch (day){
-      case 1:
-        return "Jan";
-        break;
-      case 2:
-        return "Feb";
-        break;
-      case 3:
-        return "Mar";
-        break;
-      case 4:
-        return "Apr";
-        break;
-      case 5:
-        return "May";
-        break;
-      case 6:
-        return "June";
-        break;
-      case 7:
-        return "July";
-        break;
-      case 8:
-        return "Aug";
-        break;
-      case 9:
-        return "Sept";
-        break;
-      case 10:
-        return "Oct";
-        break;
-      case 11:
-        return "Nov";
-        break;
-      case 12:
-        return "Dec";
-        break;
-      default:
-        return "Not Valid";
-        break;
-    }
-  }
-
-  String getTitle(day){
-    return getWeekday(day.weekday) + ', ' + getMonth(day.month) + ' ' + day.day.toString();
-  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: Firestore.instance.collection('posts').snapshots(),
       builder: (context, snapshot) {
-        if(snapshot.hasData && snapshot.data.documents != null && snapshot.data.documents.length > 0){
-          calculateWaste(snapshot);
-          return Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text(widget.title + ' - ' + '$wasteCount'),
-            ),
-            body: ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              itemBuilder: (content, index) {
-                var post = snapshot.data.documents[index];
-                return ListTile(
-                  title: Text(getTitle(post['date'].toDate())),
-                  trailing: Text(post['wasteAmount'].toString())
-                );
-              }
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: null,
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, 
-          );
-        } else {
-          return Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text(widget.title + ' - ' + 0.toString()),
-            ),
-            body: Center(child: CircularProgressIndicator()),
-            floatingActionButton: FloatingActionButton(
-              onPressed: null,
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-          );
-        }
+        calculateWaste(snapshot);
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(widget.title + ' - ' + '$wasteCount'),
+          ),
+          body: scaffoldBody(snapshot),
+          floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, 
+        );
       }
     );
   }
---
+++ wasteagram/lib/widgets/scaffoldBody.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+  String getWeekday(day){
+    switch (day) {
+      case 1:
+        return 'Monday';
+        break;
+      case 2:
+        return 'Tuesday';
+        break;
+      case 3:
+        return 'Wednesday';
+        break;
+      case 4:
+        return 'Thursday';
+        break;
+      case 5:
+        return 'Friday';
+        break;
+      case 6:
+        return 'Saturday';
+        break;
+      case 7:
+        return 'Sunday';
+        break;
+      default:
+        return 'Not valid';
+        break;
+    }
+  }
+
+  String getMonth(day){
+    switch (day){
+      case 1:
+        return "Jan";
+        break;
+      case 2:
+        return "Feb";
+        break;
+      case 3:
+        return "Mar";
+        break;
+      case 4:
+        return "Apr";
+        break;
+      case 5:
+        return "May";
+        break;
+      case 6:
+        return "June";
+        break;
+      case 7:
+        return "July";
+        break;
+      case 8:
+        return "Aug";
+        break;
+      case 9:
+        return "Sept";
+        break;
+      case 10:
+        return "Oct";
+        break;
+      case 11:
+        return "Nov";
+        break;
+      case 12:
+        return "Dec";
+        break;
+      default:
+        return "Not Valid";
+        break;
+    }
+  }
+
+  String getTitle(day){
+    return getWeekday(day.weekday) + ', ' + getMonth(day.month) + ' ' + day.day.toString();
+  }
+
+
+Widget scaffoldBody(AsyncSnapshot<dynamic> snapshot){
+  if(snapshot.hasData && snapshot.data.documents != null && snapshot.data.documents.length > 0){
+    return ListView.builder(
+      itemCount: snapshot.data.documents.length,
+      itemBuilder: (content, index) {
+        var post = snapshot.data.documents[index];
+        return ListTile(
+          title: Text(getTitle(post['date'].toDate())),
+          trailing: Text(post['wasteAmount'].toString())
+        );
+      }
+    );
+  } else {
+    return Center(child: CircularProgressIndicator());
+  }
+
+}
